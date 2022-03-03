d15ca8ebc93f16987c2b1f499f9f3a02e7fed5b0
Beezay Bohora <59993093+beezay@users.noreply.github.com>
2022-03-03 22:59:18 +0545

    Switching deployment branch to master

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p>This is Changed Github Personal Site</p>
+      <p>Being Deployed From Master!!</p>
     </div>
   );
 }
